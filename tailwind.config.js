ac4d2e9d27a9789bc6a663082454bf8a1b200ef4
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-12 17:09:36 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,27 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  // Les fichiers où les classes Tailwind sont utilisées
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}', // Pour les pages Next.js ou React
-    './components/**/*.{js,ts,jsx,tsx}', // Pour les composants réutilisables
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      // Ajout de nouvelles couleurs pour la personnalisation
       colors: {
-        main: '#ff0000', // Rouge principal
-        body: '#0f0f0f', // Couleur principale pour le fond
+        main: '#ff0000',
+        body: '#0f0f0f',
       },
-      // Ajout d'espacements personnalisés pour le design
       spacing: {
-        headerHeight: '8rem', // Hauteur de l'en-tête
-        headerShrinkHeight: '5rem', // Hauteur réduite de l'en-tête
+        headerHeight: '8rem',
+        headerShrinkHeight: '5rem',
       },
-      // Définition des points de rupture pour le responsive
       screens: {
-        sm: '640px', // Petit écran
-        md: '768px', // Écran moyen
-        lg: '1024px', // Grand écran
-        xl: '1280px', // Très grand écran
-        '2xl': '1536px', // Super grand écran
-        'hover-hover': { raw: '(hover: hover)' }, // Pour les appareils supportant le hover
+        'hover-hover': { raw: '(hover: hover)' },
       },
-      // Ajout de typographie pour le texte
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.body'),
-            a: {
-              color: theme('colors.main'),
-              '&:hover': {
-                color: theme('colors.main'),
-              },
-            },
-          },
-        },
-      }),
     },
   },
-  // Ajout de plugins pour des fonctionnalités avancées
   plugins: [
-    require('@tailwindcss/forms'), // Pour styliser les formulaires
-    require('@tailwindcss/typography'), // Pour gérer le texte et les articles
-    require('@tailwindcss/aspect-ratio'), // Pour gérer les ratios (ex : images responsives)
-    require('@tailwindcss/line-clamp'), // Pour limiter les lignes de texte
     plugin(function ({ addVariant }) {
-      // Ajout de variantes personnalisées
       addVariant('&>*~*', '&>*', 'not-last', '&:not(:last-child)');
     }),
   ],
