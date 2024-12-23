4a5d67171eb1ba4a36013c127065c53279fab768
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-23 16:27:21 +0100

    Update ItemGrid.tsx

--- components/ItemGrid.tsx
+++ components/ItemGrid.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ItemGrid = ({ items }: Props) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {items.map((item) => (
         <ItemCard key={item.id} item={item} />
       ))}
