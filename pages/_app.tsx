f52b5d18b13d1333a145e67af2cc02b4e3d5f3c0
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-23 16:15:11 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,5 +22,17 @@ function MyApp({ Component, pageProps }: AppProps) {
     </Provider>
   );
 }
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default MyApp;
