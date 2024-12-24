a3196a01471eeff8e76575eab546af11a34388ca
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-24 22:54:57 +0100

    Update Button.tsx

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -4,20 +4,19 @@ import classNames from 'classnames';
 interface Props {
   onClick?: () => void;
   children: ReactNode;
-  variant?: 'watchNow' | 'watchTrailer';
   className?: string;
+  outline?: boolean; // Ajout de la propriété outline
 }
 
-const Button = ({ onClick, children, variant, className }: Props) => {
+const Button = ({ onClick, children, className, outline = false }: Props) => {
   const baseStyles = 'px-6 py-2 rounded-md font-bold transition';
-  const variantStyles = {
-    watchNow: 'bg-main text-white hover:bg-opacity-90',
-    watchTrailer: 'border-2 border-main text-main hover:bg-main hover:text-white',
-  };
+  const outlineStyles = outline
+    ? 'border-2 border-main text-main hover:bg-main hover:text-white'
+    : 'bg-main text-white hover:bg-opacity-90';
 
   return (
     <button
-      className={classNames(baseStyles, variant && variantStyles[variant], className)}
+      className={classNames(baseStyles, outlineStyles, className)}
       onClick={onClick}
     >
       {children}
