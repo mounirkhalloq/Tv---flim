14a2783a8493ed22984586975fdd4d51c9f8f237
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-24 23:42:55 +0100

    Update Button.tsx

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,23 +1,25 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
 
-// Interface des propriétés pour le composant Button
 interface ButtonProps {
-  onClick?: () => void; // Gestionnaire d'événements pour le clic
-  children: ReactNode; // Contenu du bouton
-  className?: string; // Classes CSS supplémentaires
-  outline?: boolean; // Style bordure
-  small?: boolean; // Taille compacte
+  onClick?: () => void;
+  children: ReactNode;
+  className?: string;
+  outline?: boolean;
+  small?: boolean;
 }
 
-const Button = ({ onClick, children, className, outline = false, small = false }: ButtonProps) => {
-  // Styles de base
+const Button = ({
+  onClick,
+  children,
+  className,
+  outline = false,
+  small = false,
+}: ButtonProps) => {
   const baseStyles = 'px-6 py-2 rounded-md font-bold transition';
-  // Styles pour le mode "outline"
   const outlineStyles = outline
     ? 'border-2 border-main text-main hover:bg-main hover:text-white'
     : 'bg-main text-white hover:bg-opacity-90';
-  // Styles pour la taille
   const sizeStyles = small ? 'px-4 py-1 text-sm' : 'px-6 py-2 text-base';
 
   return (
