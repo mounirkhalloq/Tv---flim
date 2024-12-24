75022a591252a7f53334c3513175c22c60233441
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-24 23:29:04 +0100

    Update ItemSlider.tsx

--- components/Slider/ItemSlider.tsx
+++ components/Slider/ItemSlider.tsx
@@ -1,29 +1,35 @@
-import { ReactNode } from 'react';
-import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import { Button } from '@/components';
+import ItemList from './ItemList';
+import { MovieItemProps } from '@/model/movie';
 
 interface Props {
-  onClick?: () => void;
-  children: ReactNode;
-  className?: string;
-  outline?: boolean; // Propriété pour gérer les boutons avec bordure
-  small?: boolean; // Propriété pour gérer les petits boutons
+  title: string;
+  items: MovieItemProps[];
 }
 
-const Button = ({ onClick, children, className, outline = false, small = false }: Props) => {
-  const baseStyles = 'px-6 py-2 rounded-md font-bold transition';
-  const outlineStyles = outline
-    ? 'border-2 border-main text-main hover:bg-main hover:text-white'
-    : 'bg-main text-white hover:bg-opacity-90';
-  const sizeStyles = small ? 'px-4 py-1 text-sm' : 'px-6 py-2 text-base';
+const ItemSlider = ({ title, items }: Props) => {
+  const router = useRouter();
 
   return (
-    <button
-      className={classNames(baseStyles, outlineStyles, sizeStyles, className)}
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <div className="px-2 sm:px-4 md:px-6 mb-6">
+      <div className="flex items-center justify-between mb-6">
+        <h2 className="text-lg sm:text-xl md:text-2xl font-bold">
+          {title}
+        </h2>
+        {items.length > 0 && (
+          <Button
+            small
+            outline
+            onClick={async () => await router.push(items[0].media_type)}
+          >
+            View More
+          </Button>
+        )}
+      </div>
+      <ItemList items={items} />
+    </div>
   );
 };
 
-export default Button;
+export default ItemSlider;
