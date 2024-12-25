5e49380e10e1c4548db7aff7ce49a38282880274
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-26 00:00:40 +0100

    Update Navbar.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import cn from 'classnames';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
