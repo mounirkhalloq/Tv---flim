e6f0d8387690649cbadfe2dabaa7f107c93a08e5
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-26 15:32:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -23,9 +24,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      addVariant('&>*~*', '&>*', 'not-last', '&:not(:last-child)');
-    }),
-  ],
+  plugins: [],
 };
