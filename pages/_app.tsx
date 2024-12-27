45b4f16d6835e543c67006cf793227e7826e17e7
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-27 23:59:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,12 +14,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Mobile-Responsive Meta Tag */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
+        <title>Your Website</title>
       </Head>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+
+      {/* Wrap Content in a Responsive Container */}
+      <div className="min-h-screen flex flex-col bg-gray-100">
+        <Navbar />
+        
+        {/* Main Content */}
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        
+        <Footer />
+      </div>
     </Provider>
   );
 }
