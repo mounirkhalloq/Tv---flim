cfc6b0b31a1b583bd2225b1887c3e0e54344a511
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-28 18:35:02 +0100

    Update constants.ts

--- ultis/constants.ts
+++ ultis/constants.ts
@@ -7,12 +7,18 @@ export const mediaTypes: MediaType[] = ['all', 'movie', 'tv'];
 
 export const imageOriginal = (imgPath: string) =>
   `${TMDB_IMAGE}original${imgPath}`;
-export const w500Image = (imgPath: string) => `${TMDB_IMAGE}w500${imgPath}`;
-export const embedMovie = (id: number) => `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
-export const imageResize = (src: string, dimension: string = 'w200') =>
-  `${TMDB_IMAGE}${dimension}${src}`;
+
+export const w500Image = (imgPath: string) =>
+  `${TMDB_IMAGE}w500${imgPath}`;
+
+export const embedMovie = (id: number) =>
+  `https://2embed.org/embed/movie?tmdb=${id}`;
+
 export const embedEpisode = (id: number, season: number, episode: number) =>
   `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
+
+export const imageResize = (src: string, dimension: string = 'w200') =>
+  `${TMDB_IMAGE}${dimension}${src}`;
 
 export const airDate = (firstAir: string, lastAir: string) => {
   return firstAir === lastAir ? firstAir : `${firstAir} - ${lastAir}`;
