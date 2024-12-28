bf9e7dce9f88b142382524278e484c81fa7c2357
mounirkhalloq <91810612+mounirkhalloq@users.noreply.github.com>
2024-12-28 17:06:37 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,34 @@
-const plugin = require('tailwindcss/plugin');
+import '@/styles/globals.scss';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/bundle';
 
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        main: '#ff0000',
-        body: '#0f0f0f',
-      },
-      spacing: {
-        headerHeight: '8rem',
-        headerShrinkHeight: '5rem',
-      },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-      },
-    },
-  },
-  plugins: [],
-};
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { Navbar, Footer } from '@/components';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
+        <title>TV Film</title>
+      </Head>
+
+      {/* Force background black and text white */}
+      <div style={{ backgroundColor: '#000', color: '#fff' }} className="min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </Provider>
+  );
+}
+
+export default MyApp;
